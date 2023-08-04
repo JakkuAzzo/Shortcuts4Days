6f2eab3e308b80c44498471c1d36af8269f1cb1f
Nathan <46282855+NathanBrownBennett@users.noreply.github.com>
2023-08-04 21:14:23 +0100

    DangerousCalc3.py

---
+++ Dangerous Calc Series/DangerousCalc3.py
@@ -0,0 +1,21 @@
+import subprocess
+
+print('''
+      Welcome to DangerousCalc3!
+      The last Dangerous Calc was vulnerable to code injection, in two ways:
+      Through eval() and through pickle.
+      Enter a math expression, 
+      we'll evaluate it without eval(), exec(), conditionals, or pickle.
+      Still refuse to use multiple conditions, so we'll use subprocess.run() instead.
+      Try enter an expression like '1 + 1' :
+      ''')
+
+input_str = str(input("Enter a calculation expression such as '1+1' or '2*3-4' : "))
+
+try:
+    process = subprocess.run(["python3", "-c", f"{input_str}"], shell=True, capture_output=True, text=True, check=True)
+    result = process.stdout.strip()
+    print(f"Result: {result}")
+except subprocess.CalledProcessError as e:
+    print("Error occurred while evaluating the expression.")
+    print(e)
