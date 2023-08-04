753f3e91433cab5aa6392a8e50716db26a9b8cff
Nathan <46282855+NathanBrownBennett@users.noreply.github.com>
2023-08-04 16:58:28 +0100

    DangerousCalc2.py

---
+++ Dangerous Calc Series/DangerousCalc2.py
@@ -0,0 +1,29 @@
+
+import pickle
+
+print('''
+      Dangerous Calc's eval() function had terrible consequences, 
+      since eval() can execute any code, not just math expressions. 
+      Infuriating, but nevermind, we'll just have to write another, 
+      safer calculator that can only do math without bothering with
+      multiple conditions.
+      eASY pEASY, rIGHT?
+      ''')
+
+input_str = str(input("Enter a calculation expression such as '1+1' : "))
+
+# Pickle the input expression
+with open('expression.pkl', 'wb') as file:
+    pickle.dump(input_str, file)
+
+# Unpickle the expression
+with open('expression.pkl', 'rb') as file:
+    expression = pickle.load(file)
+
+    # Use ast.literal_eval() for safer evaluation
+    print(expression, "=", exec(expression))
+
+
+
+
+  
